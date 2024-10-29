30f1fb2de1842238a5ef2ec141742623c03a9b33
Kirill Golovanov <465565@niuitmo.ru>
2024-10-29 20:54:43 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,8 +3,9 @@ import math
 
 def area(r):
     return math.pi * r * r
-
+    # Функция для вычисления площади круга по радиусу
 
 def perimeter(r):
     return 2 * math.pi * r
+    # Функция для вычисления периметра круга по радиусу
 
